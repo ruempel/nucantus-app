63a4438e0a5a3a5c2f02eed58a652e4605ee42ea
Andreas Rümpel <ruempel@gmail.com>
2022-04-29 23:04:26 +0200

    Optimize app code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { SongListComponent } from './song-list/song-list.component';
-import { ChallengeListComponent } from './challenge-list/challenge-list.component';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {SongListComponent} from './song-list/song-list.component';
+import {ChallengeListComponent} from './challenge-list/challenge-list.component';
 
 const routes: Routes = [
-    { path: 'songs', component: SongListComponent },
-    { path: 'challenges', component: ChallengeListComponent },
-    { path: '', component: SongListComponent }
+  {path: 'songs', component: SongListComponent},
+  {path: 'challenges', component: ChallengeListComponent},
+  {path: '', component: SongListComponent}
 ];
 
 @NgModule({
-    imports: [ RouterModule.forRoot(routes) ],
-    exports: [ RouterModule ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule {
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,43 +1,45 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
-import { SongListComponent } from './song-list/song-list.component';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatTableModule } from '@angular/material/table';
-import { MatButtonModule } from '@angular/material/button';
-import { ChallengeListComponent } from './challenge-list/challenge-list.component';
-import { MatIconModule } from '@angular/material/icon';
-import { NameInputDialogComponent } from './name-input-dialog/name-input-dialog.component';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatInputModule } from '@angular/material/input';
-import { FormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule} from '@angular/forms';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTableModule} from '@angular/material/table';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatInputModule} from '@angular/material/input';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {ChallengeListComponent} from './challenge-list/challenge-list.component';
+import {SongListComponent} from './song-list/song-list.component';
+import {NameInputDialogComponent} from './name-input-dialog/name-input-dialog.component';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        SongListComponent,
-        ChallengeListComponent,
-        NameInputDialogComponent
-    ],
-    imports: [
-        BrowserModule,
-        HttpClientModule,
-        BrowserAnimationsModule,
-        MatToolbarModule,
-        MatTableModule,
-        MatButtonModule,
-        MatIconModule,
-        MatFormFieldModule,
-        MatInputModule,
-        FormsModule,
-        MatDialogModule,
-        AppRoutingModule
-    ],
-    bootstrap: [ AppComponent ]
+  declarations: [
+    AppComponent,
+    SongListComponent,
+    ChallengeListComponent,
+    NameInputDialogComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatToolbarModule,
+    MatTableModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDialogModule,
+    AppRoutingModule
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule {
 }
